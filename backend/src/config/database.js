c69723b4952c6750e5dadc2de7cbaa02c3495e00
Victor Martins <vitor_jhoul@hotmail.com>
2026-02-26 16:55:51 -0500

    deploy config

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,23 +1,15 @@
 const { Sequelize } = require("sequelize");
 
-let sequelize;
-
-if (process.env.NODE_ENV === "production") {
-  sequelize = new Sequelize({
-    dialect: "sqlite",
-    storage: "./database.sqlite"
-  });
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      dialect: "mysql"
-    }
-  );
-}
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+    logging: false
+  }
+);
 
 module.exports = sequelize;
